da47e9cb66e592e9d3e3b2b29ff006ab9ce1feb1
Kabadzh0b <chubbudda@gmail.com>
2023-08-18 15:15:16 +0300

    navigation, 1st screen

--- App.tsx
+++ App.tsx
@@ -1,23 +1,19 @@
-import {StyleSheet, View} from 'react-native';
 import {Provider} from "react-redux";
 import {store} from "./store";
-import PostList from "./components/PostList";
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import PostList from "./Screens/PostList";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
     return (
         <Provider store={store}>
-            <View style={styles.container}>
-                <PostList></PostList>
-            </View>
+            <NavigationContainer>
+                <Stack.Navigator initialRouteName="Posts">
+                    <Stack.Screen name="Posts" component={PostList}/>
+                </Stack.Navigator>
+            </NavigationContainer>
         </Provider>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
--- Screens/PostList.tsx
+++ Screens/PostList.tsx
@@ -2,7 +2,7 @@ import {FlatList, StyleSheet, Text, View} from "react-native";
 import {useTypedSelector} from "../hooks/useTypedSelector";
 import {useEffect} from "react";
 import {fetchPosts} from "../store/action-creators/fetchPosts";
-import Post from "./Post";
+import Post from "../components/Post";
 import {useDispatch} from "react-redux";
 
 
@@ -17,23 +17,30 @@ export default function PostList() {
     const {posts, error, loading} = useTypedSelector(state => state.post);
 
     if (loading) {
-        return <View><Text>Loading</Text></View>
+        return <View><Text style={styles.messages}>Loading</Text></View>
     }
 
     if (error) {
-        return <View><Text>Error</Text></View>
+        return <View><Text style={styles.messages}>Error</Text></View>
     }
 
     return (
-        <View style={styles.list}>
-            <Text style={styles.header}>Posts</Text>
-            <FlatList data={posts} renderItem={({item}) => <Post title={item.title} body={item.body} id={item.id}/>}
-                      keyExtractor={(item) => item.id.toString()}></FlatList>
+        <View style={styles.container}>
+            <View style={styles.list}>
+                <FlatList data={posts} renderItem={({item}) => <Post title={item.title} body={item.body} id={item.id}/>}
+                          keyExtractor={(item) => item.id.toString()}></FlatList>
+            </View>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
     list: {
         flex: 1,
         marginTop: 36,
@@ -42,5 +49,10 @@ const styles = StyleSheet.create({
         textAlign:"center",
         fontWeight:"bold",
         fontSize:20,
+    },
+    messages:{
+        textAlign:"center",
+        fontSize:50,
+        fontWeight:"bold"
     }
 })
